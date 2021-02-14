ce354badec9d17752711aa2b8b02df5920bd7a6a
Richard <Eidr@sheridancollege.ca>
2021-02-14 10:05:49 -0500

    ICE 1

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -5,7 +5,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
  *
  * @author Ronak
  * Richard Eid
- * 991365145
+ *  Student: 991365145
  */
 public class CardTrick {
     public static void main(String[] args)
